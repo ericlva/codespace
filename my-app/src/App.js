531764e0312abdd5d10505ea44c4c64985d36b93
盟主zuo <ericbyz@gmail.com>
2022-06-17 03:25:10 +0000

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
   
+  fetch("www.ctrip.com")
   return (
     <div className="App">
       <header className="App-header">
-        <p>debug</p>
+        <p>codespaces demo</p>
         <button type="button" onClick={() =>{
           setCount((count) => count + 1)
         }}>
